bbfe00d9aad462d44e5612b44bf4bb5672a22107
Lenfree Yeung <lenfree.yeung@gmail.com>
2017-07-24 10:10:10 +1000

    Add instructions how to delete kafka topics

--- README.md
+++ README.md
@@ -9,3 +9,22 @@ listTopics
 
 This returns all topics except for specific names.
 
+```bash
+$ cp env.example .env
+```
+
+Usage:
+======
+
+```bash
+$ go run listTopics.go
+```
+
+To delete topics:
+
+```bash
+$  go run listTopics.go > listTopics.txt
+$ cat listTopics.txt|while read line; do
+    kafka-topics --zookeeper 127.0.0.1:2183/<cluster_name> --delete --topic $line;
+  done
+```
--- listTopics.go
+++ listTopics.go
@@ -3,31 +3,40 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/Shopify/sarama"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("%s\n", "Error loading .env file")
+		os.Exit(1)
+	}
+	filterTopic := os.Getenv("filterTopic")
+
 	config := sarama.NewConfig()
 	hosts := []string{"localhost:9092"}
 	client, err := sarama.NewClient(hosts, config)
 	defer client.Close()
 	if err != nil {
-		log.Panicln(err)
+		fmt.Printf("%s\n", err.Error())
 	}
 
 	topics, err := client.Topics()
 	log.Printf("Number of topics #: %+#v\n", len(topics))
 	if err != nil {
-		log.Panicln(err)
+		fmt.Printf("%s\n", err.Error())
 	}
 
 	brokers := client.Brokers()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err.Error())
 	}
-	log.Printf("Number ofbrokers #: %+#v\n", len(brokers))
+	log.Printf("0 ofbrokers #: %+#v\n", len(brokers))
 
 	broker := sarama.NewBroker("localhost:9092")
 	err = broker.Open(nil)
@@ -36,15 +45,14 @@ func main() {
 	req := sarama.MetadataRequest{Topics: topics}
 	metadataResponse, err := broker.GetMetadata(&req)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err.Error())
 	}
 
-	log.Printf("Number of res topics #: %+#v\n", len(metadataResponse.Topics))
+	fmt.Printf("Number of res topics #: %+#v\n", len(metadataResponse.Topics))
 
 	topicName := make(chan string)
-	var filterTopic string
-	filterTopic = "lo-werphat"
-	for i := 1; i < 30; i++ {
+
+	for i := 1; i < 10; i++ {
 		go printTopics(topicName, filterTopic, i)
 	}
 
@@ -57,15 +65,18 @@ func printTopics(topics chan string, filterTopic string, routine int) {
 	// TODO: use regex instead
 	for topic := range topics {
 		if strings.Contains(topic, "_samza_checkpoint_ver_1_for_") && !strings.Contains(topic, filterTopic) {
-			log.Printf("Got %s === goroutine %d\n", topic, routine)
+			fmt.Printf("%s\n", topic)
+			//log.Printf("Got %s === goroutine %d\n", topic, routine)
 		}
 
 		if strings.Contains(topic, "_samza_coordinator_") && !strings.Contains(topic, filterTopic) {
-			log.Printf("Got %s === goroutine %d\n", topic, routine)
+			fmt.Printf("%s\n", topic)
+			//log.Printf("Got %s === goroutine %d\n", topic, routine)
 		}
 
 		if strings.Contains(topic, "--samza-store") && !strings.Contains(topic, filterTopic) {
-			log.Printf("Got %s === goroutine %d\n", topic, routine)
+			fmt.Printf("%s\n", topic)
+			//log.Printf("Got %s === goroutine %d\n", topic, routine)
 		}
 	}
 }
